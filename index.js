c06007e0a33fab33faa8e127fef2d4c3f255d645
casca <8927157+casca@users.noreply.github.com>
2020-08-04 13:53:39 +0200

    cluster fork

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express = require('express');
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+const cluster = require('cluster');
+const crypto = require('crypto');
+const { profileEnd } = require('console');
+
+if (cluster.isMaster) {
+  cluster.fork();
+  // cluster.fork();
+} else {
+  const express = require('express');
+  const app = express();
+
+  // function doWork(duration) {
+  //   const start = Date.now();
+  //   while (Date.now() - start < duration) {}
+  // }
+
+  app.get('/', (_req, res) => {
+    // doWork(5000);
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+      res.send('Hi there');
+    });
+  });
+
+  app.get('/fast', (req, res) => res.send('This was fast!'));
+
+  app.listen(3000);
 }
-
-app.get('/', (_req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000);
