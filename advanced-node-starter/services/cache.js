473eb4fc5f5689bd7d0d20f2d1db9cd65e1cac45
casca <8927157+casca@users.noreply.github.com>
2020-08-24 13:54:01 +0200

    create a more universal mongodb query cache key

--- advanced-node-starter/services/cache.js
+++ advanced-node-starter/services/cache.js
@@ -1,8 +1,26 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisURL = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisURL);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-  console.log("I'm about to run a query");
-  return exec.apply(this, arguments);
+mongoose.Query.prototype.exec = async function () {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name }),
+  );
+  // console.log(key);
+
+  const cacheValue = await client.get(key);
+  if (cacheValue) {
+    console.log(cacheValue);
+    return JSON.parse(cacheValue);
+  }
+
+  const result = await exec.apply(this, arguments);
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };
