31a4d710c28ef1e7c55afcce16c8faf98803d57e
casca <8927157+casca@users.noreply.github.com>
2020-08-28 21:42:13 +0200

    use puppeteer to start headless chromium

--- advanced-node-starter/tests/header.test.js
+++ advanced-node-starter/tests/header.test.js
@@ -1,5 +1,19 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two numbers', () => {
   const sum = 1 + 2;
 
   expect(sum).toEqual(3);
+});
+
+test('We can launch a browser', async () => {
+  jest.setTimeout(60000);
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto('localhost:3000');
+
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
 });
