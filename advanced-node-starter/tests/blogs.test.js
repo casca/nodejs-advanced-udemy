ead98708bc8422b2f47783cdd6dc1138930233c9
casca <8927157+casca@users.noreply.github.com>
2020-09-12 10:59:34 +0200

    implement more tests

--- advanced-node-starter/tests/blogs.test.js
+++ advanced-node-starter/tests/blogs.test.js
@@ -22,4 +22,42 @@ describe('When logged in', () => {
 
     expect(label).toEqual('Blog Title');
   });
+
+  describe('And using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My Title');
+      await page.type('.content input', 'My Content');
+      await page.click('form button');
+    });
+
+    test('submitting takes user to review  screen', async () => {
+      const text = await page.getContentsOf('h5');
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    test('submitting then saving adds blog to index page', async () => {
+      await page.click('button .green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      expect(title).toEqual('My Title');
+
+      const content = await page.getContentsOf('p');
+      expect(content).toEqual('My Content');
+    });
+  });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      expect(titleError).toEqual('You must provide a value');
+
+      const contentError = await page.getContentsOf('.content .red-text');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
 });
