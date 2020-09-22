1dd2a79252434f180c74ba577e3a861ccf69c538
casca <8927157+casca@users.noreply.github.com>
2020-09-22 08:29:39 +0200

    add image upload form

--- advanced-node-starter/client/src/components/blogs/BlogFormReview.js
+++ advanced-node-starter/client/src/components/blogs/BlogFormReview.js
@@ -7,6 +7,7 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  state = { file: null };
   renderFields() {
     const { formValues } = this.props;
 
@@ -25,10 +26,7 @@ class BlogFormReview extends Component {
 
     return (
       <div>
-        <button
-          className="yellow darken-3 white-text btn-flat"
-          onClick={onCancel}
-        >
+        <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
           Back
         </button>
         <button className="green btn-flat right white-text">
@@ -47,11 +45,19 @@ class BlogFormReview extends Component {
     submitBlog(formValues, history);
   }
 
+  onFileChange(event) {
+    this.setState({ file: event.target.files });
+    console.log(event.target.files);
+  }
+
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an image</h5>
+        <input type="file" accept="image/*" onChange={this.onFileChange.bind(this)} />
 
         {this.renderButtons()}
       </form>
